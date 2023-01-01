d5440af62e9c7fafb77dc02b592fe9ac79d4a6eb
Per Rawdin <perrawd@gmail>
2023-01-01 21:01:46 +0100

    Add InMemoryUserDetailsManager method

--- src/main/java/org/reactbb/ts/config/SecurityConfig.java
+++ src/main/java/org/reactbb/ts/config/SecurityConfig.java
@@ -1,18 +1,33 @@
 package org.reactbb.ts.config;
 
+import org.reactbb.ts.utils.PropertyUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager user() throws IOException {
+        return new InMemoryUserDetailsManager(User
+                .withUsername(PropertyUtil.getProperties().getProperty("app.base.user"))
+                .password(PropertyUtil.getProperties().getProperty("app.base.pass"))
+                .authorities(PropertyUtil.getProperties() .getProperty("app.base.authorities"))
+                .build()
+        );
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
