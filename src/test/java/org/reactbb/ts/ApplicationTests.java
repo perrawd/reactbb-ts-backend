8eea04332affceacc4bbc66e870322449bd396a0
Per Rawdin <perrawd@gmail>
2023-01-01 16:36:07 +0100

    Initial commit

---
+++ src/test/java/org/reactbb/ts/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reactbb.ts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
